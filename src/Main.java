a885b6d72b4be36f6845e820810671fc90de8aa4
Christopher Pace <Christopher_pace@ymail.com>
2018-07-24 13:36:19 -0500

    Initial checkin

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        ArrayList<String> squad = new ArrayList<String>();
+        squad.add("Cody");
+        squad.add("Carlos");
+        squad.add("PJ");
+        squad.add("JP");
+        System.out.println(squad);
     }
 }
